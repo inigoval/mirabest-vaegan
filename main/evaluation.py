6a3ad6c27783dd5ce6e4270998cec8095631e05a
inigo val <inigoval@gmail.com>
2020-11-25 19:07:36 +0000

    cpu error fix

--- main/evaluation.py
+++ main/evaluation.py
@@ -35,11 +35,12 @@ def class_idx(y):
     hybrid_idx = np.where(y>7.5)
     return fri_idx, frii_idx, hybrid_idx
 
-def inception_score(G, n_samples=1000, eps = 1E-10):
+def inception_score(G, n_z, n_samples=1000, eps = 1E-10):
     # I = torch.load()
     Z = torch.randn(n_samples, n_z).cuda().view(n_samples, n_z, 1, 1)
-    X_gen = G(Z)
-    # y_gen = 
+    X = G(Z)
+    # p_y|x = I(X)
+    # p_y = torch.mean(p_y|
 
 def umap_map():
     X, y = dset_array()
