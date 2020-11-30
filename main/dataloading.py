fixed std and mean values for normalisation

main/dataloading.py
@@ -174,7 +174,7 @@ Args:
 def load_data(batch_size):
     transform = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize(mean=[0.5], std=[0.5])
+    torchvision.transforms.Normalize(mean=[0.0031], std=[0.0352])
     ])
     train_data = MiraBest_full(DATA_PATH, train=True, transform=transform, download=True)
     trainLoader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)

main/networks.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import os
 
 # latent vector length
-n_z = 2
+n_z = 32
 
 # feature map sizes
 n_ef = 16

main/run.py
@@ -33,7 +33,7 @@ BCE_loss = nn.BCELoss(reduction='mean')
 ## initialise parameters ##
 batch_size = 32
 batch_size_test = 1000
-n_z = 2
+n_z = 32
 n_epochs = 1000
 gamma = 1  # weighting for style (L_llike) in generator loss function
 # smoothing parameters