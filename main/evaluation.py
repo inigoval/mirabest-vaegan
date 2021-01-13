400b3e5892d9071ec179f7fe3500bc4288a0cd52
Inigo Val <inigoval@gmail.com>
2021-01-13 16:21:09 +0000

    push to run

--- main/evaluation.py
+++ main/evaluation.py
@@ -63,10 +63,7 @@ def inception_score(I, X, eps = 1E-10):
     #X = renormalize(X)
     p_yx = I(X)
     p_y = torch.mean(p_yx, 0).view(1, -1)
-    print(p_y)
     p_y = p_y.expand(X.size()[0], -1)
-    print(p_y.size())
-    print(p_y)
     KL = torch.mean(p_yx * (torch.log(p_yx + eps) - torch.log(p_y + eps))).detach().cpu().numpy()
     # squeeze inception score between 0 and 1
     #IS = KL/3
